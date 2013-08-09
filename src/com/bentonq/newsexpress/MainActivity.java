370477e1e293a563e031d33bd6e4d21ade498d02
Bentonq Yi <bentonq@gmail.com>
2013-08-09 18:17:13 +0800

    Test news item layout

--- src/com/bentonq/newsexpress/MainActivity.java
+++ src/com/bentonq/newsexpress/MainActivity.java
@@ -1,23 +1,54 @@
 package com.bentonq.newsexpress;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(new BaseAdapter() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+			@Override
+			public int getCount() {
+				return 10;
+			}
+
+			@Override
+			public long getItemId(int position) {
+				return position;
+			}
+			
+			@Override
+			public Object getItem(int position) {
+				return null;
+			}
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				LayoutInflater inflater = LayoutInflater
+						.from(MainActivity.this);
+				View view = inflater.inflate(R.layout.view_item, parent, false);
+				return view;
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
